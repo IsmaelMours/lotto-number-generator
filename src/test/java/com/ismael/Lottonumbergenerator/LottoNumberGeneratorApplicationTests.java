8aaec0b39d8b582b177cce1ca616d171b35f6fb3
Ismael Mours <83817080+IsmaelMours@users.noreply.github.com>
2024-02-14 08:47:34 +0200

    Add files via upload

---
+++ src/test/java/com/ismael/Lottonumbergenerator/LottoNumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismael.Lottonumbergenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoNumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
