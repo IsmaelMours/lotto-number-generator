951d5d9f46436a3b568ce791bcfb57060615d6de
ismaelmours <monnapuleismaelmours@gmail.com>
2024-02-14 09:37:17 +0200

    The changes are made on controller

--- src/main/java/com/ismael/Lottonumbergenerator/controller/LottoController.java
+++ src/main/java/com/ismael/Lottonumbergenerator/controller/LottoController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ismael.Lottonumbergenerator.controller;
 import com.ismael.Lottonumbergenerator.model.LottoNumberGenerator;
 import org.springframework.web.bind.annotation.*;
@@ -42,48 +41,3 @@ public class LottoController {
         lottoNumberGenerator.resetPlayedNumbers();
     }
 }
-=======
-package com.ismael.Lottonumbergenerator.controller;
-import com.ismael.Lottonumbergenerator.model.LottoNumberGenerator;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@CrossOrigin("http://localhost:3000/")
-public class LottoController {
-
-    private final LottoNumberGenerator lottoNumberGenerator;
-
-    public LottoController(LottoNumberGenerator lottoNumberGenerator) {
-        this.lottoNumberGenerator = lottoNumberGenerator;
-    }
-
-    @GetMapping("/generateUniqueNumber")
-    public int generateUniqueNumber(
-            @RequestParam int min,
-            @RequestParam int max,
-            @RequestParam List<Integer> excludeList) {
-        return lottoNumberGenerator.generateUniqueNumber(min, max, excludeList);
-    }
-
-    @GetMapping("/generateMultipleLottoNumbers")
-    public List<List<Integer>> generateMultipleLottoNumbers(
-            @RequestParam int min,
-            @RequestParam int max,
-            @RequestParam int numberOfSets,
-            @RequestParam int numbersPerSet) {
-        return lottoNumberGenerator.generateMultipleLottoNumbers(min, max, numberOfSets, numbersPerSet);
-    }
-
-    @PostMapping("/addPlayedNumbers")
-    public void addPlayedNumbers(@RequestBody List<Integer> numbers) {
-        lottoNumberGenerator.addPlayedNumbers(numbers);
-    }
-
-    @GetMapping("/resetPlayedNumbers")
-    public void resetPlayedNumbers() {
-        lottoNumberGenerator.resetPlayedNumbers();
-    }
-}
->>>>>>> d2631f827d67afff3573446c986db7cb3c191288
